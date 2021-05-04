0fd923e1a468a8dc2cd642c36e2e83f6f5800fb7
Alok <alokranjan5111998@gmail.com>
2021-05-04 17:38:42 +0530

    Intial Commit

---
+++ src/lockdown/traveller/LockdownTraveller.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lockdown.traveller;
+
+/**
+ *
+ * @author hp
+ */
+public class LockdownTraveller
+{
+
+    public static void main(String[] args) 
+    {
+       Dashboard ds=new Dashboard();
+       ds.setVisible(true);
+    }
+    
+}
